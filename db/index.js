a5c9f47790dc60efbd0295c408c573fa9c46ba70
Demi Zayas <this.journey.dsz@gmail.com>
2023-02-01 20:36:35 -0800

    Another rework on client

--- db/index.js
+++ db/index.js
@@ -1,9 +1,6 @@
 const { Client } = require('pg');
 // const client = new Client('postgres://localhost:5432/juicebox-dev');
-const client = new Client({
-    connectionString: process.env.DATABASE_URL || 'postgres://localhost:5432/juicebox-dev',
-    ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : undefined,
-  })
+const client = new Client( process.env.DATABASE_URL || 'postgres://localhost:5432/juicebox-dev' )
 
 //USER METHODS
 
